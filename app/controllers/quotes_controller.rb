36c0d83ee4206915a1f0b86c57dd989d0fd71d90
Jordan Hudgens <jordan@wow.com>
2015-02-27 19:31:49 -0600

    Built out different ways of managing the request/response calls

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,26 +1,37 @@
 class QuotesController < Rulers::Controller
+  def a_quote
+    render :a_quote, :noun => :winking
+  end
+
+  def quote_1
+    quote_1 = FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
 
   def index
     quotes = FileModel.all
     render :index, :quotes => quotes
   end
 
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
+  end
+
   def new_quote
     attrs = {
       "submitter" => "web user",
-      "quote" => "A picture is worth one k pixels",
+      "quote" => "A picture is worth a thousand pixels.",
       "attribution" => "Me"
     }
     m = FileModel.create attrs
     render :quote, :obj => m
   end
 
-  def a_quote
-    render :a_quote, :noun => :winking
-  end
+  def post_test
+    raise "NOT A POST!" unless env["REQUEST_METHOD"] == "POST"
 
-  def quote_1
-    quote_1 = Rulers::Model::FileModel.find(1)
-    render :quote, :obj => quote_1
+    "Params: #{request.params.inspect}"
   end
 end
